8511aa0e22e8a2f2acd0e3d9da1ec8dd98966eeb
袁杨 <yuanyang@uvoplus.com>
2018-02-06 11:52:00 +0800

    1.0.11

---
+++ src/run.js
@@ -0,0 +1,27 @@
+import which from 'which';
+
+function runCmd(cmd, args, fn) {
+  args = args || [];
+  var runner = require('child_process').spawn(cmd, args, {
+    // keep color
+    stdio: "inherit"
+  });
+  runner.on('close', function (code) {
+    if (fn) {
+      fn(code);
+    }
+  });
+}
+
+function findNpm() {
+  var npms = process.platform === 'win32' ? ['tnpm.cmd', 'cnpm.cmd', 'npm.cmd'] : ['tnpm', 'cnpm', 'npm'];
+  for (var i = 0; i < npms.length; i++) {
+    try {
+      which.sync(npms[i]);
+      console.log('use npm: ' + npms[i]);
+      return npms[i];
+    } catch (e) {
+    }
+  }
+  throw new Error('please install npm');
+}
